007194fc008ba1674555168fcc83491f7ab62e19
dyeongkim <113990279+dyeongkim@users.noreply.github.com>
2023-03-11 15:06:57 +0900

    Update boj_10026.py

--- BOJ_baekjoon/boj_10026.py
+++ BOJ_baekjoon/boj_10026.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 
 def bfs(x,y, color):
     queue = deque()
-    graph[x][y] = 0
+    vis[x][y] = True
     queue.append((x,y))
     while queue :
         x,y = queue.popleft()
@@ -15,48 +15,39 @@ def bfs(x,y, color):
             if nx < 0 or nx >= N or ny < 0 or ny >= N:
                 continue
             
-            if graph[nx][ny] == 0:
+            if vis[nx][ny] == True:
                 continue
 
             if graph[nx][ny] == color :
-                graph[nx][ny] = 0
+                vis[nx][ny] = True
                 queue.append((nx,ny))
-    res.append(1)
-
-def bfs_RG(x,y, color):
-    queue = deque()
-    graph[x][y] = 0
-    queue.append((x,y))
-    while queue :
-        x,y = queue.popleft()
-        for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-
-            if nx < 0 or nx >= N or ny < 0 or ny >= N:
-                continue
-            
-            if graph[nx][ny] == 0:
-                continue
-
-            if graph[nx][ny] == color :
-                graph[nx][ny] = 0
-                queue.append((nx,ny))
-    res.append(1)
-
 
 N = int(input())
 graph = []
+vis = [[False]*N for _ in range(N)]
 dx, dy = [-1,0,1,0],[0,1,0,-1]
-res = []
-res_RG = []
+cnt, cnt_RG = 0, 0
 for i in range(N):
     graph.append(list(input().strip()))
 
 for i in range(N):
     for j in range(N):
-        if graph[i][j] != 0:
+        if vis[i][j] == False :
             bfs(i,j, graph[i][j])
+            cnt += 1
+            
 
-print(len(res), end= ' ')
-print(len(res_RG))
+
+vis = [[False]*N for _ in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == "G" :
+            graph[i][j] = "R"
+
+for i in range(N):
+    for j in range(N):
+        if vis[i][j] == False :
+            bfs(i,j, graph[i][j])
+            cnt_RG += 1
+print(cnt, cnt_RG)
