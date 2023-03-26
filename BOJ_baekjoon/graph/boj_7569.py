09fcf2485da739e32fb0980167f2a59641e5fbaf
dyeongkim <dyeongkim98@gmail.com>
2023-03-26 23:53:22 +0900

    7569 작업중

--- BOJ_baekjoon/graph/boj_7569.py
+++ BOJ_baekjoon/graph/boj_7569.py
@@ -3,9 +3,6 @@ from collections import deque
 input = sys.stdin.readline
 
 def bfs(z, y, x) :
-    queue = deque()
-    queue.append((z,y,x))
-
     while queue :
         z, y, x = queue.popleft()
 
@@ -17,36 +14,44 @@ def bfs(z, y, x) :
             if nx < 0 or ny < 0 or nz < 0 or nx >= M or ny >= N or nz >= H:
                 continue
 
-            if graph[nz][ny][nx] != 0 :
+            if graph[nz][ny][nx] == -1 :
                 continue
-            
-            graph[nz][ny][nx] = graph[z][y][x] + 1 
-            queue.append((nz,ny,nx))
+
+            elif graph[nz][ny][nx] == 0 :
+                graph[nz][ny][nx] = graph[z][y][x] + 1 
+                queue.append((nz,ny,nx))
+
+            elif graph[nz][ny][nx] > graph[z][y][x] + 1  :
+                graph[nz][ny][nx] = graph[z][y][x] + 1 
+                queue.append((nz,ny,nx))
 
 
 
 M, N, H = map(int, input().split())
 dx, dy, dz =[1, 0, -1, 0, 0, 0],[0, 1, 0, -1, 0, 0],[0, 0, 0, 0, 1, -1]
 graph = []
+queue = deque()
 
 for i in range(H):
     temp = []
     for j in range(N):
         temp.append(list(map(int,input().split())))
+        for k in range(M):
+            if temp[j][k] == 1:
+                queue.append((i,j,k))
     graph.append(temp)
 
-for z in range(H):
-    for y in range(N):
-        for x in range(M):
-            if graph[z][y][x] == 1 :
-                bfs(z,y,x)
+z, y, x = queue.popleft()
+bfs(z,y,x)
 
 result = []
 
-for z in range(H):
-    for y in range(N):
+for z in graph:
+    for y in z:
         for x in y:
             if x == 0 :
                 print(-1)
                 exit(0)
-    
+        result.append(max(y))
+
+print(max(result)-1)
