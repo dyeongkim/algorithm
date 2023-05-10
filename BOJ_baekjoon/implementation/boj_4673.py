c8b7290ca28fa514526ebb9737f9cfede0a817d3
dyeongkim <dyeongkim98@gmail.com>
2023-05-11 00:55:28 +0900

    업데이트

---
+++ BOJ_baekjoon/implementation/boj_4673.py
@@ -0,0 +1,16 @@
+# 백준 4673 -  셀프 넘버
+def selfnum(n):
+    temp = n
+    for i in str(n):
+        temp += int(i)
+    if temp <= 10000:
+        result[temp] = False
+
+
+result = [True]*10001
+for i in range(1,10001):
+    selfnum(i)
+
+for i in range(1,10001):
+    if result[i]:
+        print(i)
