5575b249890945baa85a93873619d796197e23d7
dyeongkim <dyeongkim98@gmail.com>
2023-05-19 11:59:08 +0900

    업데이트

--- BOJ_baekjoon/implementation/boj_1966.py
+++ BOJ_baekjoon/implementation/boj_1966.py
@@ -1,12 +1,21 @@
 # 백준 1966 - 프린터 큐
 from collections import deque
 
-K = int(input())
+K = int(input()) # 테스트 케이스
 
 for i in range(K):
-    N, M = map(int,input().split())
-    queue = deque(map(int, input().split()))
+    N, M = map(int,input().split()) # 문서 개수, 원하는 문서 위치
+    queue = deque(map(int, input().split())) # 문서 중요도
+    idx = deque(list(range(N))) # 문서 순서
+    result = 0 # 출력 카운트
 
-    print(queue)
     while queue :
-        print(queue.popleft())
+        if max(queue) == queue[0] : # 큐에서 가장 큰 수가 가장 앞에 있을때
+            result += 1 # 출력 카운트
+            queue.popleft() # 큐 출력
+            q_idx = idx.popleft() # 현재 출력된 큐 원래 위치
+            if q_idx == M : # 원하는 문서 순서 체크
+                print(result) #출력
+        else : # 제일 큰 수가 아니면 뒤로 다시 넣기
+            queue.append(queue.popleft()) 
+            idx.append(idx.popleft())
--- BOJ_baekjoon/implementation/boj_2108.py
+++ BOJ_baekjoon/implementation/boj_2108.py
@@ -1,6 +1,19 @@
 # 백준 2108 - 통계학
+import math
+
+def mean(li) :
+    return sum(li)/len(li)
+
+def median(li) :
+    len
+    
+def mode(li) :
+    
+def ran(li) :
+    
 
 N = int(input())
 li = []
 for i in range(N):
     li.append(int(input()))
+
