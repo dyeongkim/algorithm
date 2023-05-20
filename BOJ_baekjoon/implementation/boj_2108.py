80bd40343ea6c0fa679a7e3063cba433d8006a91
dyeongkim <dyeongkim98@gmail.com>
2023-05-20 22:54:27 +0900

    업데이트

--- BOJ_baekjoon/implementation/boj_2108.py
+++ BOJ_baekjoon/implementation/boj_2108.py
@@ -1,19 +1,46 @@
 # 백준 2108 - 통계학
-import math
-
+import sys
+input = sys.stdin.readline
 def mean(li) :
-    return sum(li)/len(li)
+    return round(sum(li)/len(li))
 
 def median(li) :
-    len
+    temp = li[:]
+    temp.sort()
+    return temp[len(temp)//2]
+
     
 def mode(li) :
-    
+    li.sort()
+    dic = dict()
+    for i in li :
+        if i in dic :
+            dic[i] += 1
+        else :
+            dic[i] = 1
+    max_num = max(dic.values())
+    temp = []
+    for i in dic:
+        if max_num == dic[i]:
+            temp.append(i)
+
+            
+    if len(temp) > 1:
+        return temp[1]
+    else :
+        return temp[0]
+
+
 def ran(li) :
-    
+    return max(li) - min(li)
 
 N = int(input())
+
 li = []
 for i in range(N):
     li.append(int(input()))
 
+print(mean(li))
+print(median(li))
+print(mode(li))
+print(ran(li))
