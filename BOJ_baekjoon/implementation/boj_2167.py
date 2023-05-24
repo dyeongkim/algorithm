fc83c7603d2d5feeac0d2faf00c2e9dd7a0fe47b
dyeongkim <dyeongkim98@gmail.com>
2023-05-24 16:30:49 +0900

    업데이트

--- BOJ_baekjoon/implementation/boj_2167.py
+++ BOJ_baekjoon/implementation/boj_2167.py
@@ -5,28 +5,21 @@ input = sys.stdin.readline
 
 N,M = map(int,input().split())
 li = []
+dp = [[0]*(M+1) for _ in range(N+1)]
 
 for i in range(N):
     li.append(list(map(int,input().split())))
+
+for i in range(1,N+1):
+    for j in range(1, M+1):
+        dp[i][j] = li[i-1][j-1] + dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1]
 
 K = int(input())
 
 for _ in range(K):
     i,j,x,y = map(int,input().split())
     nx, ny = i-1, j-1
-    result = 0
-    while True :
-        
-        result += li[nx][ny]
-
-        ny += 1
-        if ny >= M :
-            ny = 0
-            nx += 1
-        
-        if nx >= x or ny >= y :
-            print(result)
-            break
+    print(dp[x][y]-dp[x][j-1] - dp[i-1][y] + dp[i-1][j-1])
 
         
         
