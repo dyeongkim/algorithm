e1573942fd196e73bb96044681feb9b01d67e635
Dong <dyeongkim98@gmail.com>
2023-05-30 20:39:43 +0900

    업데이트

--- BOJ_baekjoon/implementation/boj_10828.py
+++ BOJ_baekjoon/implementation/boj_10828.py
@@ -17,9 +17,9 @@ def si():
     return len(li)
 def em():
     if len(li) > 0:
-        return False
+        return 0
     else :
-        return  True
+        return  1
 
 def to():
     if em():
@@ -30,4 +30,23 @@ def to():
 N = int(input())
 
 for _ in range(N):
-    temp = map()
+    temp = list(map(str,input().split()))
+
+    if len(temp) > 1 :
+        s, num = temp[0], int(temp[1])
+
+        pu(num)
+    else :
+        
+        if temp[0] == 'pop' :
+            print(po())
+
+        elif temp[0] == 'size' :
+            print(si())
+
+        elif temp[0] == 'empty' :
+            print(em())
+            
+        elif temp[0] == 'top' :
+            print(to())
+
