fc5328cc2dc9da744d5968fe5e3b92dbe5697e45
dyeongkim <dyeongkim98@gmail.com>
2023-06-27 23:42:19 +0900

    수정

--- BOJ_baekjoon/implementation/boj_2504.py
+++ BOJ_baekjoon/implementation/boj_2504.py
@@ -1,24 +1,41 @@
 # 백준 2504 - 괄호의 값
-import sys
-input = sys.stdin.readline
-
 S = list(input())
 
 stack = []
 result = 0
 tmp = 1
 for i in range(len(S)):
-
     if S[i] == "(":
         stack.append(S[i])
         tmp *= 2
+    
+    elif S[i] == "[":
+        stack.append(S[i])
+        tmp *= 3
 
     elif S[i] == ")":
         if not stack or stack[-1] == "[":
-            result == 0
+            result = 0
             break
-        if S[i-1] == "("
+
+        if S[i-1] == "(" :
             result += tmp
         stack.pop()
         tmp //=2
+    
+    else :
+        if not stack or stack[-1] == "(":
+            result = 0
+            break
+
+        if S[i-1] == "[":
+            result += tmp
         
+        stack.pop()
+        tmp //= 3
+
+
+if stack:
+    print(0)
+else :
+    print(result)        
