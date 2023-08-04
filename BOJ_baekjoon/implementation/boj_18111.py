259d12a8b73531a699f57a697040bc7c6d4c319f
dyeongkim <dyeongkim98@gmail.com>
2023-08-04 23:57:13 +0900

    시간초과

--- BOJ_baekjoon/implementation/boj_18111.py
+++ BOJ_baekjoon/implementation/boj_18111.py
@@ -5,6 +5,27 @@ input = sys.stdin.readline
 
 
 N, M, B = map(int, input().split())
-field = [list(map(int, input().split())) for _ in range(N)]
+field = [list(map(int, input().rstrip().split())) for _ in range(N)]
 ans = sys.maxsize
-idx = 
+idx = 0
+
+for i in range(257) :
+    UB = 0
+    TB = 0
+    for x in range(N):
+        for y in range(M):
+            if field[x][y]>1:
+                TB += field[x][y] - i
+            else :
+                UB += i - field[x][y]
+
+    if UB > TB + B:
+        continue
+
+    cnt = TB*2 + UB
+
+    if cnt <= ans:
+        ans = cnt
+        idx = i
+
+print(ans, idx)
