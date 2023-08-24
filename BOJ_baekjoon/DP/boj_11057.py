2a61205d997e0f825097da246afb68122e244a1e
dyeongkim <dyeongkim98@gmail.com>
2023-08-24 16:48:37 +0900

    업데이트

--- BOJ_baekjoon/DP/boj_11057.py
+++ BOJ_baekjoon/DP/boj_11057.py
@@ -2,4 +2,10 @@
 
 N = int(input())
 
-dp[]
+dp = [1]*10
+
+for i in range(N-1):
+    for j in range(1,10):
+        dp[j] += dp[j-1]
+
+print(sum(dp)%10007)
---
+++ BOJ_baekjoon/DP/boj_9465.py
@@ -0,0 +1,7 @@
+# 백준 9465 - 스티커
+
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    st = [[0]*n for _ in range(n)]
