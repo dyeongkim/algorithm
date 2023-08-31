aec23eda16804cfc3eca469bd957589dc6b893e6
dyeongkim <dyeongkim98@gmail.com>
2023-08-31 17:51:57 +0900

    업데이트

--- BOJ_baekjoon/DP/boj_1912.py
+++ BOJ_baekjoon/DP/boj_1912.py
@@ -1,12 +1,10 @@
 # 백준 1912 - 연속합
 
 n = int(input())
-num = list(map(int, input().split()))
-dp = [0] * n
 
-10 -4 3 1 5 6 -35 12 21 -1
-10 6 9 10 15 21 -14 -2 19 -1
-6 -1 
-for i in range(n) :
-    tot = 0
-    for j in range(i) :
+dp = list(map(int, input().split()))
+
+for i in range(1,n) :
+    dp[i] = max(dp[i], dp[i] + dp[i-1])
+
+print(max(dp))
