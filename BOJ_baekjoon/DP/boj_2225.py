75059428b87ef25425775d2ba3f8cc3de19b249a
dyeongkim <dyeongkim98@gmail.com>
2023-09-06 23:57:56 +0900

    업데이트

--- BOJ_baekjoon/DP/boj_2225.py
+++ BOJ_baekjoon/DP/boj_2225.py
@@ -1,3 +1,16 @@
 # 백준 2225 - 합분해
 
 N, K = map(int, input().split())
+
+dp = [[0]*201 for _ in range(201)]
+
+for i in range(201):
+    dp[1][i] = 1
+    dp[2][i] = i+1
+
+for i in range(2, 201):
+    dp[i][1] = i
+    for j in range(2, 201):
+        dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 1000000000
+
+print(dp[K][N])
