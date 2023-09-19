9cdd3577780d739abf1ed3bc97859a0a88269d5b
Dong <dyeongkim98@gmail.com>
2023-09-19 19:40:32 +0900

    업데이트

--- BOJ_baekjoon/boj_28114.py
+++ BOJ_baekjoon/boj_28114.py
@@ -1,2 +1,25 @@
 # 백준 28114 - 팀명 정하기
+import sys
 
+input = sys.stdin.readline
+team = []
+
+for _ in range(3):
+    a, b, c = input().split()
+    team.append((int(a), int(b), c))
+
+num = ""
+name = ""
+
+team.sort(reverse=True)
+
+for i in team :
+    name += i[2][0]
+
+team.sort(key= lambda x : x[1])
+
+for i in team :
+    num += str(i[1]%1000)
+
+print(num)
+print(name)
