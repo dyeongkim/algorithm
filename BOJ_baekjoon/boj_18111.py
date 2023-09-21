3f9236746d445d64bee677ef1bd1a5dc9a53cea2
Dong <dyeongkim98@gmail.com>
2023-09-21 21:53:59 +0900

    .

--- BOJ_baekjoon/boj_18111.py
+++ BOJ_baekjoon/boj_18111.py
@@ -5,21 +5,29 @@ input = sys.stdin.readline
 N, M, B = map(int,input().split())
 ground = []
 result = sys.maxsize
+floor = 0
 
 for _ in range(N):
-    ground.append(list(map(int, input().split())))
+    ground.append(list(map(int, input().rstrip().split())))
 
 for i in range(257):
     max_num, min_num = 0,0
     for j in range(N):
         for k in range(M):
-            if ground[j][k] >= i :
+            if ground[j][k] > i :
                 max_num += ground[j][k] - i
             else :
                 min_num += i - ground[j][k]
     
-    if max_num + B >= min_num:
-        if min_num + (max_num*2) <= result :
-            result = min_num + max_num*2
+    if min_num > max_num + B:
+        continue
+    
+    cnt = max_num + (min_num*2)
 
-print(result, i)
+    if cnt <= result :
+        result = cnt
+        floor = i
+
+print(result, floor)
+
+# https://land-turtler.tistory.com/82
