a434a27fb243e1f66a6b96c60415cde8240fc748
Dong <dyeongkim98@gmail.com>
2023-09-26 21:05:01 +0900

    .

--- BOJ_baekjoon/boj_28116.py
+++ BOJ_baekjoon/boj_28116.py
@@ -5,7 +5,7 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 num = list(map(int, input().split()))
-num_copy = num[:]
+dic = {i : 0 for i in num}
 result = [0] * N
 
 for i in range(N):
@@ -14,9 +14,11 @@ for i in range(N):
         print(num)
         if num[min_index] > num[j]:
             min_index = j
-    result[num_copy.index(num[i])] += (min_index - i)
-    result[num_copy.index(num[min_index])] += (min_index - i)
+    dic[num[min_index]] += (min_index - i)
+    dic[num[i]]  += (min_index - i)
     num[min_index], num[i] = num[i], num[min_index]
-    print(result)
+
+for x, y in dic :
+    result[x-1] = y
 
 print(result)
