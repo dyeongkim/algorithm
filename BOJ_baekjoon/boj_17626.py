1a4565d2a9e1851755a746e4b7871216e7810084
dyeongkim <dyeongkim98@gmail.com>
2023-10-18 00:54:25 +0900

    .

---
+++ BOJ_baekjoon/boj_17626.py
@@ -0,0 +1,19 @@
+# 백준 17626 - Four Squares
+import sys
+
+input = sys.stdin.readline
+minnum = sys.maxsize
+N = int(input())
+dp = [0,1]
+
+for i in range(2, N+1):
+    minnum = sys.maxsize
+    j = 1
+
+    while (j**2) <= i:
+        minnum = min(minnum, dp[i-(j**2)])
+        j += 1
+    
+    dp.append(minnum+1)
+
+print(dp[N])
