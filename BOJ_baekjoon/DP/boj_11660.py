417e1c0bdb56cd7a7d5124e78adec04481b21c9d
Dongyeong Kim <dyeongkim98@gmail.com>
2024-04-01 09:40:03 +0900

    백준 11660 완성

--- BOJ_baekjoon/DP/boj_11660.py
+++ BOJ_baekjoon/DP/boj_11660.py
@@ -28,13 +28,8 @@ dp = [[0] * (N + 1) for _ in range(N+1)]
 
 for i in range(1, N+1):
     for j in range(1, N+1):
-        if j == 1:
-            dp[i][j] = dp[i-1][-1] + num[i-1][j-1]
-        else :
-            dp[i][j] = dp[i][j-1] + num[i-1][j-1]
+            dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + num[i-1][j-1]
 
-print(dp)
 for _ in range(M):
     x1, y1, x2, y2 = map(int, input().split())
-    print(dp[x2][y2],dp[x2][y1-1],dp[x1-1][-1],dp[x1-1][y1-1])
-    print(dp[x2][y2]-dp[x2][y1-1]-dp[x1-1][-1]+dp[x1-1][y1-1])
+    print(dp[x2][y2]-dp[x2][y1-1]-dp[x1-1][y2]+dp[x1-1][y1-1])
